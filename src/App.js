806af9ee8cd932a8b42986cfd7ad283ea6946de8
tmcsoftdsolution <lovetallman426@gmail.com>
2022-05-07 15:14:08 -0400

    add logo animation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import { setup } from "./script";
 import gsap from "gsap";
-import { About } from "./components";
 
 export default function App() {
   const [step, setStep] = useState(0);
-  const [showTextLogo, setShowTextLogo] = useState(false);
-  const [showContact, setShowContact] = useState(false);
 
   useEffect(() => {
     setup();
@@ -16,22 +13,27 @@ export default function App() {
   const onHello = () => {
     console.log("onHello");
     setStep(1);
-    gsap.to("#landing-logo", {
-      height: 55,
-      duration: 1,
-      top: 50,
-      left: 50,
-      onComplete: () => {
-        console.log("complete");
-        setShowTextLogo(true);
-        setShowContact(true);
-      },
-    });
+    gsap
+      .timeline()
+      .to("#landing-logo", {
+        height: 55,
+        duration: 4,
+        rotate: 360,
+        top: 50,
+        left: 50,
+        ease: "power3.out",
+        onComplete: () => {
+          console.log("complete");
+        },
+      })
+      .to("#logo-text", { duration: 1, left: 90 }, "-=2")
+      .to("#btn-contact", { duration: 1, right: 0 }, "-=2")
+      .to("#about-container", { duration: 2, top: 0 }, "-=4");
   };
 
   const onProjects = () => {
     console.log("onProjects");
-    setStep(0);
+    // setStep(0);
   };
 
   return (
@@ -41,21 +43,25 @@ export default function App() {
       {/* Logo */}
       <img id="landing-logo" src="assets/landing-logo.png" alt="landig logo" />
       {/* Text Logo */}
-      {showTextLogo && (
-        <img id="logo-text" src="assets/logo-text.png" alt="logo text" />
-      )}
-      {showContact && (
-        <button id="btn-contact" className="btn-main">
-          CONTACT
-        </button>
-      )}
+      <img id="logo-text" src="assets/logo-text.png" alt="logo text" />
+      <button id="btn-contact" className="btn-main">
+        CONTACT
+      </button>
       {step === 0 && (
         <button id="btn-hello" className="btn-main" onClick={onHello}>
           HELLO
         </button>
       )}
       {/* About */}
-      {step === 1 && <About onProjects={onProjects} />}
+      <div id="about-container">
+        <div className="fs-2 text-white mx-5 px-5">
+          LinkDAP is a boutique web development agency specializing in design,
+          development, branding, and everything in between.
+        </div>
+        <button className="btn-main mt-5" onClick={onProjects}>
+          PROJECTS
+        </button>
+      </div>
     </div>
   );
 }
